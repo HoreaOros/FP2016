3704a8ed1d710754d9c618768f115425f4e51fb2
Horea Oros <horea.oros@gmail.com>
2016-10-18 10:34:14 +0300

    Add second enry point

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // secventa de cod de mai jos calculeaza suma 10 + 11 + 12 ... + 20 si afiseaza rezultatul.
+            int suma = 10;
+            int i = 1;
+
+            while (i <= 10)
+            {
+                suma = suma + i;
+                i = i + 1;
+            }
+
+            Console.WriteLine("Suma este: {0}", suma);
+
+        }
+    }
+}
