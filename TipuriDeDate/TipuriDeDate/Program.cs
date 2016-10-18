eda5275878ebcde904bc6fbebc08ae119a8f56fe
Horea Oros <horea.oros@gmail.com>
2016-10-18 11:58:19 +0300

    Add try block

--- TipuriDeDate/TipuriDeDate/Program.cs
+++ TipuriDeDate/TipuriDeDate/Program.cs
@@ -24,6 +24,40 @@ namespace TipuriDeDate
 
             suma = n * (n + 1) / 2;
 
+            // input
+            string line;
+            line = Console.ReadLine();
+
+
+            try
+            {
+                n = int.Parse(line);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Sirul introdus nu este in forma corespunzatoare");
+                return;
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            bool b; // valorile pe care le poate lua
+            // b sunt true sau false;
+
+
+            if (n % 2 == 0)
+            {
+                suma = n / 2 * (n + 1);
+            }
+            else
+            {
+                suma = (n + 1) / 2 * n;
+            }
+            
+
             Console.WriteLine("Suma este: {0}", suma);
         }
     }
