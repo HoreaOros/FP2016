3bbd81ea7b76d70e7a54776cb53da78b782b96ed
Horea Oros <horea.oros@gmail.com>
2016-10-18 10:58:55 +0300

    Add project TipuriDeDate. Compute Gauss sum.

---
+++ TipuriDeDate/TipuriDeDate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipuriDeDate
+{
+    class Program
+    {
+        // calculez suma numerelor de la 1 la n
+        static void Main(string[] args)
+        {
+            int n;
+            int suma = 0;
+
+            n = 10;
+
+            // Sn = 1+  2  + ... + n
+            // Sn = n+(n-1)+ ... + 1
+            // 2Sn= (n+1) + (n+1) + .. + (n+1)
+            // 2Sn = n(n+1)
+            // Sn = n(n+1)/2;
+
+            suma = n * (n + 1) / 2;
+
+            Console.WriteLine("Suma este: {0}", suma);
+        }
+    }
+}
