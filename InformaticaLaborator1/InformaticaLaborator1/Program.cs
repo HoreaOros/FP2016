6bd8b823f57353e9502b934b3c71542b32668a0c
Horea Oros <horea.oros@gmail.com>
2016-11-14 11:02:12 +0200

    Add Laborator2 Matematica

---
+++ InformaticaLaborator1/InformaticaLaborator1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformaticaLaborator1
+{
+    class Program
+    {
+        public static void Experiment1Zar(int n)
+        {
+            Random rnd = new Random();
+            int[] fq = new int[6];
+            for (int i = 0; i < n; i++)
+            {
+                fq[rnd.Next(fq.Length)]++;
+            }
+
+            for (int i = 0; i < fq.Length; i++)
+            {
+                Console.WriteLine("{0} - {1}", i + 1, fq[i]);
+            }
+        }
+        public static void HO()
+        {
+            string line1 = "**    **   *********";
+            string line2 = "**    **  ***********";
+            string line3 = "**    **  **       **";
+            string line4 = "********  **       **";
+            string line5 = "********  **       **";
+            string line6 = "********  **       **";
+            string line7 = "**    **  **       **";
+            string line8 = "**    **  ***********";
+            string line9 = "**    **   *********";
+
+            Console.WriteLine(line1);
+            Console.WriteLine(line2);
+            Console.WriteLine(line3);
+            Console.WriteLine(line4);
+            Console.WriteLine(line5);
+            Console.WriteLine(line6);
+            Console.WriteLine(line7);
+            Console.WriteLine(line8);
+            Console.WriteLine(line9);
+
+
+            // (!(a < b) && !(a > b)) <==> a == b
+            // (a >= b) && (a <= b)
+            // !(a && b) <==> !a || !b
+            // !(a || b) <==> !a && !b
+
+
+
+            
+
+        }
+        static void Main(string[] args)
+        {
+            HO();
+
+            //Asterisc9("HOREA");
+
+            int an = 1984;
+            bool bisect = AnBisect(an);
+
+
+            Experiment1Zar(1000000);
+        }
+
+        private static bool AnBisect(int an)
+        {
+            //return (an % 4 == 0 && an % 100 != 0) || an % 400 == 0;
+
+            if ((((an % 4) == 0) && ((an % 100) != 0)) || ((an % 400) == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            // i < n && a[i] == 0
+            // a[i] == 0 && i < n
+
+            // i < n && x == y++
+
+            // a || b 
+        }
+
+        private static void Asterisc9(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Laborator2/Laborator2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// 14.11.2016
+// Laborator Matematica anul I
+namespace Laborator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Distanta eculidiana dintre doua puncte P1(x,y1), P2(x2, y2)
+            float x1 = 2.3f, x2 = 5.3f;
+            float y1 = 4.1f, y2 = 8.1f;
+
+            float d12;
+
+            d12 = DistantaEuclidiana(x1, y1, x2, y2);
+            Console.WriteLine("Distanta Eculidiana dintre cele doua puncte este: {0}", d12);
+
+
+            int[] zile = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            // Anotimpuri
+            for (int m = 1; m <= 12; m++)
+                for (int d = 1; d <= zile[m]; d++)
+                {
+                    string s = Anotimp(d, m);
+                    Console.WriteLine("Ziua {0}.{1} este in anotimpul {2}", d, m, s);
+                }
+        }
+
+        private static string Anotimp(int d, int m)
+        {
+            if (m == 1 || m == 2)
+            {
+                return "iarna";
+            }
+            else if (m == 3)
+            {
+                if (d < 20)
+                    return "iarna";
+                else
+                    return "primavara";
+            }
+            else if (m == 4 || m == 5)
+            {
+                return "primavara";
+            }
+            else if (m == 6)
+            {
+                if (d < 20)
+                    return "primavara";
+                else
+                    return "vara";
+            }
+            else if (m == 7 || m == 8)
+            {
+                return "vara";
+            }
+            else if (m == 9)
+            {
+                if (d < 22)
+                    return "vara";
+                else
+                    return "toamna";
+            }
+            else if (m == 10 || m == 11)
+            {
+                return "toamna";
+            }
+            else if (m == 12)
+            {
+                if (d < 21)
+                    return "toamna";
+                else
+                    return "iarna";
+            }
+            else
+                return "date gresite";
+        }
+
+        private static float DistantaEuclidiana(float x1, float y1, float x2, float y2)
+        {
+            double distanta = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+
+            return (float)distanta;
+        }
+    }
+}
