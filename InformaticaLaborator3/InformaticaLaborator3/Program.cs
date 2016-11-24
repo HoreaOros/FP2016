be79315a7a1915a4062f29240be1ba16f7b0c27c
Horea Oros <horea.oros@gmail.com>
2016-11-24 13:55:37 +0200

    Add lab 3 informatica

---
+++ InformaticaLaborator3/InformaticaLaborator3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformaticaLaborator3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, k = 1000, n = 17;
+
+            int r = Pow(a, k, n); // (a ^ k) (mod n) = ((a mod n)... (a mod n))(mod n)
+            Console.WriteLine("{0} ^ {1} (mod {2}) = {3}", a, k, n, r);
+
+            r = PowFast(a, k, n);
+            Console.WriteLine("{0} ^ {1} (mod {2}) = {3}", a, k, n, r);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+
+        private static int PowFast(int a, int k, int n)
+        {
+            if (k == 0)
+            {
+                return 1;
+            }
+            else if (k % 2 == 1)
+            {
+                return (a * PowFast(a, k - 1, n)) % n;
+            }
+            else
+            {
+                int r = PowFast(a, k / 2, n);
+                return (r * r) % n;
+            }
+        }
+
+        private static int Pow(int a, int k, int n)
+        {
+            int r = 1;
+            for (int i = 1; i <= k; i++)
+            {
+                r = ((r % n) * (a % n)) % n;
+            }
+            return r;
+        }
+    }
+}
